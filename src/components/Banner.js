569ecf957052cd3278524c2fab642807b6d0d792
VSCRemy <126096029+VSCRemy@users.noreply.github.com>
2023-12-09 14:18:34 -0800

    Home page and first drop down

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,16 @@
 import React from "react";
-import Image from "../assets/img/house-banner.png";
+import Image from "../assets/img/house-banner.jpg";
 import Search from "../components/Search";
 
 const Banner = () => {
   return (
-    <section>
-      <div>
-        <div>
-          <h1>
-            <span>Rent</span> Your Dream House With Us.
+    <section className="h-full max-h-[640px] mb-8 xl:mb-24">
+      <div className="flex flex-col lg:flex-row">
+        <div className="lg:ml-8 xl:ml-[135px] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0">
+          <h1 className="text-4xl lg:text-[58px] font-semibold leading-none mb-6">
+            <span className="text-sky-500">Rent</span> Your Dream Home With Us.
           </h1>
-          <p>
+          <p className="max-w-[480px] mb-8">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -20,7 +20,11 @@ const Banner = () => {
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
         </div>
+        <div className="hidden flex-1 lg:flex justify-end items-end">
+          <img src={Image} alt="" className="rounded-2xl" />
+        </div>
       </div>
+      <Search />
     </section>
   );
 };
--- src/components/HouseContext.js
+++ src/components/HouseContext.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from "react";
+import { housesData } from "../data";
 
-const HouseContext = () => {
-  return <div>HouseContext</div>;
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({ children }) => {
+  const [houses, setHouses] = useState(housesData);
+  const [country, setCountry] = useState("Location (any)");
+  const [countries, setCountries] = useState([]);
+  const [property, setProperty] = useState("Property type (any)");
+  const [properties, setProperties] = useState([]);
+  const [price, setPrice] = useState("Property range (any)");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const allCountries = houses.map((house) => {
+      return house.country;
+    });
+    const uniqueCountries = ["Location (any)", ...new Set(allCountries)];
+
+    setCountries(uniqueCountries);
+  }, []);
+
+  return (
+    <HouseContext.Provider
+      value={{
+        country,
+        setCountry,
+        countries,
+        property,
+        setProperty,
+        properties,
+        price,
+        setPrice,
+        houses,
+        loading,
+      }}
+    >
+      {children}
+    </HouseContext.Provider>
+  );
 };
 
-export default HouseContext;
+export default HouseContextProvider;
